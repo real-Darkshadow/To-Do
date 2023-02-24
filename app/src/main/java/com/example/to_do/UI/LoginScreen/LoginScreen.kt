d716f6b8d94b754784073e3ebf44f606a5153a2b
real-Darkshadow <aadarsh.21bcan262@jecrcu.edu.in>
2023-02-24 15:13:15 +0530

    Firebase auth implemented

--- app/src/main/java/com/example/to_do/UI/LoginScreen/LoginScreen.kt
+++ app/src/main/java/com/example/to_do/UI/LoginScreen/LoginScreen.kt
@@ -2,17 +2,22 @@ package com.example.to_do.UI.LoginScreen
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.to_do.R
+import com.example.to_do.databinding.FragmentLoginScreenBinding
+import com.example.to_do.databinding.FragmentRegisterScreenBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginScreen : Fragment() {
 
-    companion object {
-        fun newInstance() = LoginScreen()
-    }
+    private lateinit var auth: FirebaseAuth
+    private var _binding: FragmentLoginScreenBinding?=null
+    private val binding get()=_binding!!
 
     private lateinit var viewModel: LoginScreenViewModel
 
@@ -20,13 +25,47 @@ class LoginScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login_screen, container, false)
+        _binding=FragmentLoginScreenBinding.inflate(inflater, container, false)
+        val view=binding.root
+        binding.signupButton.setOnClickListener{
+            findNavController().navigate(R.id.action_loginScreen_to_registerScreen)
+        }
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginScreenViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerevents()
     }
+
+    private fun registerevents() {
+        binding.loginButton.setOnClickListener {
+            val email=binding.username.text.toString()
+            val pass=binding.password.text.toString().trim()
+            if (email.isNotEmpty()&& pass.isNotEmpty()){
+                    auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener({
+                        if(it.isSuccessful){
+                            findNavController().navigate(R.id.action_loginScreen_to_homeScreen)
+                        }
+                        else{
+                            Log.d("hell",it.exception.toString())
+                        }
+                    })
+            }
+        }
+    }
+
+    private fun init(view: View) {
+        auth=FirebaseAuth.getInstance()
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+
 
 }
--- app/src/main/java/com/example/to_do/UI/RegisterScreen/RegisterScreen.kt
+++ app/src/main/java/com/example/to_do/UI/RegisterScreen/RegisterScreen.kt
@@ -2,31 +2,74 @@ package com.example.to_do.UI.RegisterScreen
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.to_do.R
+import com.example.to_do.databinding.FragmentRegisterScreenBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterScreen : Fragment() {
 
-    companion object {
-        fun newInstance() = RegisterScreen()
-    }
-
+    private lateinit var auth:FirebaseAuth
+    private var _binding:FragmentRegisterScreenBinding?=null
+    private val binding get()=_binding!!
     private lateinit var viewModel: RegisterScreenViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register_screen, container, false)
+        _binding=FragmentRegisterScreenBinding.inflate(inflater, container, false)
+        val view=binding.root
+        binding.loginButton.setOnClickListener{
+            findNavController().navigate(R.id.action_registerScreen_to_loginScreen)
+        }
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(RegisterScreenViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerevents()
     }
+
+    private fun registerevents() {
+        binding.signupButton.setOnClickListener {
+            val email=binding.username.text.toString()
+            val pass=binding.password.text.toString().trim()
+            val confirmpass=binding.confirmpassword.text.toString().trim()
+            if (email.isNotEmpty()&& pass.isNotEmpty()&& confirmpass.isNotEmpty()){
+                if(pass.equals(confirmpass)){
+                    binding.progressbar.visibility=View.VISIBLE
+                    auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener({
+                        if(it.isSuccessful){
+                            findNavController().navigate(R.id.action_registerScreen_to_homeScreen)
+                        }
+                        else{
+                            Log.d("hell",it.exception.toString())
+                        }
+                        binding.progressbar.visibility=View.GONE
+                    })
+                }
+            }
+        }
+    }
+
+    private fun init(view: View) {
+        auth=FirebaseAuth.getInstance()
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+
 
 }
