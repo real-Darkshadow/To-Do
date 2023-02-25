6cfcaaa372d3b0f034448e9596e9c1d43f419be2
real-Darkshadow <aadarsh.21bcan262@jecrcu.edu.in>
2023-02-25 10:26:19 +0530

    Todo Dialog implemented

--- app/src/main/java/com/example/to_do/UI/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/to_do/UI/HomeScreen/HomeScreen.kt
@@ -8,26 +8,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.to_do.R
+import com.example.to_do.UI.popup
+import com.example.to_do.databinding.FragmentHomeScreenBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
-class HomeScreen : Fragment() {
+class HomeScreen : Fragment(), popup.DialogNextButtonClickListener {
 
-    companion object {
-        fun newInstance() = HomeScreen()
-    }
 
     private lateinit var viewModel: HomeScreenViewModel
+    private var bindingg:FragmentHomeScreenBinding?=null
+    private val binding get()=bindingg!!
+    private lateinit var  auth:FirebaseAuth
+    private  lateinit var databaseref:DatabaseReference
+    private lateinit var popUpfrag:popup
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        bindingg=FragmentHomeScreenBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeScreenViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        registerevents()
+
     }
+
+    private fun registerevents() {
+        binding.addbutton.setOnClickListener {
+            popUpfrag= popup()
+            popUpfrag.setlistener(this)
+            popUpfrag.show(childFragmentManager,"popupfragment")
+        }
+    }
+
+    private fun init(view: View) {
+        auth=FirebaseAuth.getInstance()
+        databaseref=FirebaseDatabase.getInstance().reference
+
+    }
+
+    override fun onSaveTask(todo: String, todoet: TextInputEditText, description: String) {
+        TODO("Not yet implemented")
+    }
+
 
 }
